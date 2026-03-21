121f992f4338c26b5d1e47e95b73c989f0e1ba08
Fabio Mendes <fabiojmendes@gmail.com>
2026-03-20 22:53:57 -0400

    fix: deployment

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+version=$(git describe --tags --exact-match || "")
+
+# Regex: Starts with v, then digits.digits.digits
+if [[ $version =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Match"
+  npx wrangler deploy
+else
+  echo "No match"
+fi
