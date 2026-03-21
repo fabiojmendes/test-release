9a9780aa1ba55633a12757d50897ad09654cdb09
Fabio Mendes <fabiojmendes@gmail.com>
2026-03-20 23:10:45 -0400

    fix: update build script

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 set -xeuo pipefail
 
-git describe --tags
+git describe --tags || true
 
-version=$(git describe --tags --exact-match || echo "")
+version=$(git describe --tags --exact-match || true)
 
 # Regex: Starts with v, then digits.digits.digits
 if [[ $version =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
